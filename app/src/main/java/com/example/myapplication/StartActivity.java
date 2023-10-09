a1c73e38fb60e064e038e595e546ec76ef998053
aneeshs04 <aneeshseem@gmail.com>
2023-10-09 14:08:26 -0400

    edited errors with starting screen animation

--- app/src/main/java/com/example/myapplication/StartActivity.java
+++ app/src/main/java/com/example/myapplication/StartActivity.java
@@ -6,8 +6,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.bumptech.glide.Glide;
+
+import java.time.Instant;
 
 
 public class StartActivity extends AppCompatActivity {
