c42285e19da86e83bca7a7872be5b711c3ece99b
Danny <dannyzeng1166@gmail.com>
2023-10-22 23:48:27 -0400

    added testSetWall and testWallCollide

--- app/src/main/java/com/example/myapplication/model/Wall.java
+++ app/src/main/java/com/example/myapplication/model/Wall.java
@@ -10,6 +10,33 @@ public class Wall {
         this.bottom = bottom;
     }
 
+    public float getLeft() {
+        return left;
+    }
+    public float getRight() {
+        return right;
+    }
+    public float getTop() {
+        return top;
+    }
+    public float getBottom() {
+        return bottom;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+    public void setRight(int right) {
+        this.right = right;
+    }
+    public void setTop(int top) {
+        this.top = top;
+    }
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+
+
     public boolean collidesWith(int x, int y) {
         return x > left && x < right && y > top && y < bottom;
     }
--- app/src/test/java/com/example/myapplication/Sprint3UnitTests.java
+++ app/src/test/java/com/example/myapplication/Sprint3UnitTests.java
@@ -3,7 +3,10 @@ package com.example.myapplication;
 import static org.junit.Assert.assertEquals;
 
 
+import android.view.KeyEvent;
 import com.example.myapplication.model.Player;
+import com.example.myapplication.model.Wall;
+import com.example.myapplication.viewmodels.MainGameActivity;
 import com.example.myapplication.views.MainActivity;
 import com.example.myapplication.views.MoveLeftStrategy;
 import com.example.myapplication.views.MoveRightStrategy;
@@ -12,6 +15,8 @@ import com.example.myapplication.views.MoveDownStrategy;
 import com.example.myapplication.views.MovementStrategy;
 
 import org.junit.Test;
+
+import java.security.Key;
 
 public class Sprint3UnitTests {
     @Test
@@ -52,5 +57,26 @@ public class Sprint3UnitTests {
         player.setMovementStrategy(strategy);
         player.performMovement();
         assertEquals(initialY + 50.0, player.getY(), 0);
+    }
+
+    @Test
+    public void testSetWall() {
+        Wall wall = new Wall(150, 80, 1050, 150);
+        wall.setLeft(0);
+        wall.setRight(2000);
+        wall.setTop(50);
+        wall.setBottom(100);
+
+        assertEquals( 0, wall.getLeft(), 0);
+        assertEquals(2000, wall.getRight(), 0);
+        assertEquals(50, wall.getTop(), 0);
+        assertEquals(100, wall.getBottom(), 0);
+    }
+
+    @Test
+    public void testWallCollide() {
+        Wall wall = new Wall(150, 80, 1050, 150);
+        assertEquals(true, wall.collidesWith(500, 100));
+        assertEquals(false, wall.collidesWith(900, 200));
     }
 }
