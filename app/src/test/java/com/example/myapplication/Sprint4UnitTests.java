e3c0fb899cf6514b13ed2e5fccf6664f23216aef
aneeshs04 <aneeshseem@gmail.com>
2023-11-12 23:48:40 -0500

    14 junits total

--- app/src/test/java/com/example/myapplication/Sprint4UnitTests.java
+++ app/src/test/java/com/example/myapplication/Sprint4UnitTests.java
@@ -13,12 +13,18 @@ import com.example.myapplication.model.ChortFactory;
 import com.example.myapplication.model.Enemy;
 import com.example.myapplication.model.EnemyFactory;
 
+import com.example.myapplication.model.ImpFactory;
+import com.example.myapplication.model.MoveDownStrategy;
+import com.example.myapplication.model.MoveLeftStrategy;
+import com.example.myapplication.model.MoveRightStrategy;
+import com.example.myapplication.model.MoveUpStrategy;
+import com.example.myapplication.model.NecromancerFactory;
 import com.example.myapplication.model.Player;
 
 
 public class Sprint4UnitTests {
     @Test
-    public void testCollision() {
+    public void testPlayerEnemyCollision() {
         Player player = Player.getInstance();
         player.setX(500);
         player.setY(500);
@@ -26,6 +32,69 @@ public class Sprint4UnitTests {
         player.registerObserver(enemy);
         player.notifyObservers();
         assertEquals(enemy.contactWithPlayer(), true);
+    }
+
+    @Test
+    public void testEnemiesHaveDifferentMovementPatterns() {
+        EnemyFactory necroDemonFactor = new NecromancerFactory();
+        Enemy necromancerDemon = necroDemonFactor.createEnemy(100, 100);
+        EnemyFactory chortDemonFactory = new ChortFactory();
+        Enemy chortDemon = chortDemonFactory.createEnemy(100, 100);
+        necromancerDemon.move();
+        chortDemon.move();
+        assertNotEquals(chortDemon.getX(), necromancerDemon.getX());
+        assertNotEquals(chortDemon.getY(), necromancerDemon.getY());
+    }
+
+    @Test
+    public void testEnemiesHaveDifferentAttributes() {
+        EnemyFactory bigDemonFactory = new BigDemonFactory();
+        Enemy bigDemon = bigDemonFactory.createEnemy(100, 100);
+        EnemyFactory chortDemonFactory = new ChortFactory();
+        Enemy chortDemon = chortDemonFactory.createEnemy(100, 100);
+        EnemyFactory impDemonFactory = new ImpFactory();
+        Enemy impDemon = impDemonFactory.createEnemy(100, 100);
+        EnemyFactory necroDemonFactor = new NecromancerFactory();
+        Enemy necromancerDemon = necroDemonFactor.createEnemy(100, 100);
+        assertTrue(bigDemon.getMovementSpeed() != chortDemon.getMovementSpeed());
+        assertTrue(impDemon.getSize() != necromancerDemon.getSize());
+    }
+
+    @Test
+    public void testEnemyMovementWithinScreenBounds() {
+        Player player = Player.getInstance();
+        EnemyFactory bigDemonFactory = new BigDemonFactory();
+        Enemy bigDemon = bigDemonFactory.createEnemy(100, 100);
+        EnemyFactory chortDemonFactory = new ChortFactory();
+        Enemy chortDemon = chortDemonFactory.createEnemy(100, 100);
+        bigDemon.move();
+        assertTrue(bigDemon.getX() >= 0 && bigDemon.getY() >= 0);
+        chortDemon.move();
+        assertTrue(chortDemon.getX() >= 0 && chortDemon.getY() >= 0);
+    }
+
+    @Test
+    public void testRealTimeHPUpdateOnCollision() {
+        Player player = Player.getInstance();
+        EnemyFactory bigDemonFactory = new BigDemonFactory();
+        Enemy bigDemon = bigDemonFactory.createEnemy(100, 100);
+        player.setHealth(100);
+        player.setX(bigDemon.getX());
+        player.setY(bigDemon.getY());
+        player.notifyObservers();
+        player.setX(500);
+        player.setY(500);
+        BigDemon enemy = new BigDemon(500, 500);
+        player.registerObserver(enemy);
+        assertEquals(player.getHealth(), 100);
+    }
+
+
+    @Test
+    public void testPlayerEnemyNoCollision() {
+        Player player = Player.getInstance();
+        Enemy enemy = new BigDemon((int) player.getX() + 100, (int) player.getY() + 100);
+        assertFalse(enemy.contactWithPlayer());
     }
 
     @Test
