38c1ce05eb6f9e5a9f9857e15aa3367fbaa31e53
ajagannath6@gatech.edu <ajagannath6@gatech.edu>
2023-11-26 20:12:04 -0500

    movement space

--- app/src/main/java/com/example/myapplication/model/MoveRightStrategy.java
+++ app/src/main/java/com/example/myapplication/model/MoveRightStrategy.java
@@ -4,6 +4,7 @@ import com.example.myapplication.model.MovementStrategy;
 import com.example.myapplication.model.Player;
 
 public class MoveRightStrategy implements MovementStrategy {
+
     @Override
     public void move(Player player) {
         player.setX(player.getX() + 50);
