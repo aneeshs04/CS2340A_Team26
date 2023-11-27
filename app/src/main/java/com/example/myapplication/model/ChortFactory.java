344a731f0f483eb84973d0ac0c00aba1fd8ebc9e
ajagannath6@gatech.edu <ajagannath6@gatech.edu>
2023-11-26 21:10:29 -0500

    space in chort factory

--- app/src/main/java/com/example/myapplication/model/ChortFactory.java
+++ app/src/main/java/com/example/myapplication/model/ChortFactory.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.model;
 
 public class ChortFactory extends EnemyFactory {
+
     @Override
     public Enemy createEnemy(int x, int y) {
         return new ChortDemon(x, y);
