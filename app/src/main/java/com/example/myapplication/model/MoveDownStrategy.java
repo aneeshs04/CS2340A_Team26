d718fe2318c59d8dc95dcd9a79c5671887196b91
ajagannath6@gatech.edu <ajagannath6@gatech.edu>
2023-11-26 20:15:28 -0500

    move down space

--- app/src/main/java/com/example/myapplication/model/MoveDownStrategy.java
+++ app/src/main/java/com/example/myapplication/model/MoveDownStrategy.java
@@ -4,8 +4,10 @@ import com.example.myapplication.model.MovementStrategy;
 import com.example.myapplication.model.Player;
 
 public class MoveDownStrategy implements MovementStrategy {
+
     @Override
     public void move(Player player) {
         player.setY(player.getY() + 50);
     }
+
 }
